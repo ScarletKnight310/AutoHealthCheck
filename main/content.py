e30cd1247e2c8f5536c76f0ffeb079fb748b647b
Kashod Cagnolatti <kcagnolatti@callutheran.edu>
2022-02-01 13:13:53 -0800

    updated options to reflect the Health Check Form changes
    - added
    isSick -> Are you sick? (was true or false, now has 3 options)  (Yes / Yes, but Cleared / No)
    isExposed -> Known exposure to someone with COVID-19?  (Yes / Yes, but Cleared / No)
    isPosiTest -> Tested positive for COVID-19 (Yes / Yes, but Cleared / No)

--- main/content.py
+++ main/content.py
@@ -1,5 +1,29 @@
-url = "https://myclu.callutheran.edu/health-check/?_=1"
-num = "3122854330"
+url = "https://myclu.callutheran.edu/health-check/?_=1"  # Health check url, don't change
+brLoadTime = 10
+
+# PERSONAL INFO
+#   Keep username and password empty if saved and autofilled in browser
+#   not gonna save my login info in a public github, NO THANK YOU
+username = ""
+password = ""
+
+# FORM INFO
+num = "3122854330"  # give me a call ;)
 firstN = "Kashod"
 lastN = "Cagnolatti"
-brLoadTime = 10
+
+#   0 = Is on Campus
+#   1 = Will be on Campus
+#   2 = Will NOT be on Campus
+onCampusStatus = 0
+
+#   True = Yes
+#   False = No
+isVacc = True
+
+#   0 = Yes
+#   1 = Yes, but Cleared
+#   2 = No
+isSick = 2  # ---- Are you sick?
+isExposed = 2  # ---- Known exposure to someone with COVID-19
+isPosiTest = 2  # ---- Tested positive for COVID-19 (Yes / Yes, but Cleared / No)
--- main/main.py
+++ main/main.py
@@ -1,14 +1,16 @@
 import time
-from pynput import keyboard as kb
 import content as c
 import pyautogui as auto
 import webbrowser
 
-webbrowser.open(c.url, 1)
-
+webbrowser.open(c.url, 2)
 time.sleep(c.brLoadTime)  # Delay to open page, login
+
+auto.press('right')
+auto.write(c.username)
 auto.press('tab')
-auto.press('tab')
+auto.press('right')
+auto.write(c.password)
 auto.press('tab')
 auto.press('enter')
 
@@ -20,24 +22,61 @@ auto.write(c.num)
 auto.press('tab')
 
 # On campus?
-auto.press('enter')
+if c.onCampusStatus == 0:
+    auto.press('enter')
 auto.press('tab')
+if c.onCampusStatus == 1:
+    auto.press('enter')
 auto.press('tab')
-
+if c.onCampusStatus == 2:
+    auto.press('enter')
 auto.press('tab')
 
 # Status
-auto.press('enter')
+if c.isVacc:
+    auto.press('enter')
+    auto.press('tab')
+else:
+    auto.press('tab')
+    auto.press('enter')
+auto.press('tab')
+
+# ---- Are you sick? (Yes / Yes, but Cleared / No)
+if c.isSick == 0:
+    auto.press('enter')
+auto.press('tab')
+if c.isSick == 1:
+    auto.press('enter')
+auto.press('tab')
+if c.isSick == 2:
+    auto.press('enter')
 auto.press('tab')
 auto.press('tab')
+
+# ---- Known exposure to someone with COVID-19 (Yes / Yes, but Cleared / No)
+if c.isExposed == 0:
+    auto.press('enter')
 auto.press('tab')
-auto.press('enter')
+if c.isExposed == 1:
+    auto.press('enter')
+auto.press('tab')
+if c.isExposed == 2:
+    auto.press('enter')
 auto.press('tab')
 auto.press('tab')
-auto.press('enter')
+
+# ---- Tested positive for COVID-19 (Yes / Yes, but Cleared / No)
+if c.isPosiTest == 0:
+    auto.press('enter')
+auto.press('tab')
+if c.isPosiTest == 1:
+    auto.press('enter')
+auto.press('tab')
+if c.isPosiTest == 2:
+    auto.press('enter')
+auto.press('tab')
 
 # Sign and Confirm
-auto.press('tab')
 auto.press('space')
 auto.press('tab')
 auto.press('tab')
@@ -49,4 +88,4 @@ auto.write(c.lastN)
 
 # Finish
 auto.press('tab')
-# auto.press('enter')
+auto.press('enter')
